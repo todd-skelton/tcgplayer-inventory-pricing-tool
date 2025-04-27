18cde66e0471ee6744678c8760d9e96e79a35ec6
Todd Skelton <todd.skelton@outlook.com>
2025-04-26 20:55:02 -0500

    Remove menu button that does nothing

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -303,14 +303,6 @@ export default function Home() {
     <Stack spacing={2}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             TCGplayer Inventory Pricing Tool
           </Typography>
